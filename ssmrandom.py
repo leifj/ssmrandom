e29c7a45a7d0f5d0afaff38669d6683c5377394a
Leif Johansson <leifj@sunet.se>
2012-08-13 13:04:30 +0200

    random sleep on recv

--- ssmrandom.py
+++ ssmrandom.py
@@ -8,6 +8,7 @@ import base64
 import logging
 import getopt
 import sys
+import random
 import time
 
 if not hasattr(socket, 'IP_MULTICAST_TTL'):
@@ -21,12 +22,11 @@ ENTROPY_DEVICE='/dev/urandom'
 RNGD_PIPE = "/var/run/mc-socket"
 
 if sys.argv[1] == 'recv':
-    opts, args = getopt.getopt(sys.argv[2:], 'g:i:p:o:d')
+    opts, args = getopt.getopt(sys.argv[2:], 'g:i:p:o:L:')
     opts = dict(opts)
     opts.setdefault('-i', '0.0.0.0')
     opts.setdefault('-p', SSM_PORT)
-    opts.setdefault('-o',  RNGD_PIPE)
-    opts.setdefault('-d',False)
+    opts.setdefault('-o', RNGD_PIPE)
     opts.setdefault('-g', SSM_GROUP)
     opts.setdefault('-L',"WARNING")
     loglevel = getattr(logging, opts['-L'].upper(), None)
@@ -49,12 +49,16 @@ if sys.argv[1] == 'recv':
         logging.debug("Starting...")
         while True:
             try:
+                print "."
                 msg = json.loads(s.recv(4096))
                 data = base64.b64decode(msg['d'])
                 logging.debug(msg)
-                logging.info("Received %d bytes" % len(msg))
+                logging.info("received %d bytes" % len(msg))
                 #print "%d bytes" % len(data)
                 fd.write(data)
+                z = random.randint(1,20)
+                logging.debug("sleeping %d seconds..." % z)
+                time.sleep(z)
             except Exception,ex:
                 logging.warning(ex)
                 pass
