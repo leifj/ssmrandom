08a20dc5a9cf12da3bcb86fbb98d1d4774a37e87
Leif Johansson <leifj@sunet.se>
2012-08-15 14:03:21 +0200

    help and refactor

--- ssmrandom.py
+++ ssmrandom.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env python
-from logging.handlers import SysLogHandler
+"""
+Usage: ssmrandom {recv|send|rawsend} [options]+ [host IP(only for recv)]
+       Common options:
+        -h  print this message
+        -v  print version information
+        -g SSM multicast group (in 232.0.0.0/8)
+        -i local bind address
+        -p port
+
+        recv options:
+        -s buffer size
+        -o ouput PIPE
+
+        send and rawsend options:
+
+        -t TTL
+        -s number of bytes of entropy payload
+        -r input entropy device
+
+
+ssmramdom can be operated in either send or receive mode. In send mode it will
+read data from the input entropy device and will transmit it framed (except when
+using rawsend) as JSON objects on a multicast group in SSM address space. In
+receive mode ssmrandom will receive a random sample (using random sleep intervals
+between 1 and 20 seconds) of such SSM messages and will write the entropy
+payload to a PIPE where it can be consumed by rngd from the rng-tools package.
+
+BUGS: only ipv4 is supported
+
+NOTE that you may need to enable igmpv3 on your network for SSM to work.
+"""
 
 __author__ = 'leifj'
 
@@ -12,6 +42,7 @@ import getopt
 import sys
 import random
 import time
+from logging.handlers import SysLogHandler
 
 if not hasattr(socket, 'IP_MULTICAST_TTL'):
     setattr(socket, 'IP_MULTICAST_TTL', 33)
@@ -41,22 +72,56 @@ def _setup_logging(opts):
     logging.root.addHandler(handler)
     logging.root.setLevel(loglevel)
 
-if sys.argv[1] == 'recv':
-    opts, args = getopt.getopt(sys.argv[2:], 'g:i:p:o:L:s:')
+def usage():
+    print __doc__
+
+opts = {}
+args = []
+flags = None
+if sys.argv[1] in ('recv'):
+    flags = 'h:L:vg:i:p:o:s:'
+elif sys.argv[1] in ('send','rawsend'):
+    flags = 'h:L:vt:s:g:p:r:s:'
+else:
+    usage()
+    sys.exit()
+
+try:
+    opts, args = getopt.getopt(sys.argv[2:], flags)
     opts = dict(opts)
-    opts.setdefault('-i','0.0.0.0')
-    opts.setdefault('-p',SSM_PORT)
-    opts.setdefault('-o',RNGD_PIPE)
-    opts.setdefault('-g',SSM_GROUP)
-    opts.setdefault('-L',LOGLEVEL)
+except getopt.GetoptError, err:
+    print str(err)
+    usage()
+    sys.exit(2)
+
+if '-h' in opts:
+    usage()
+    sys.exit()
+
+if '-v' in opts:
+    print "ssmrandom version %s (c) NORDUnet 2012" % VERSION
+    sys.exit()
+
+opts.setdefault('-i','0.0.0.0')
+opts.setdefault('-p',SSM_PORT)
+opts.setdefault('-o',RNGD_PIPE)
+opts.setdefault('-g',SSM_GROUP)
+opts.setdefault('-L',LOGLEVEL)
+opts.setdefault('-r',ENTROPY_DEVICE)
+opts.setdefault('-L',LOGLEVEL)
+opts.setdefault('-t',MCTTL)
+
+group = opts['-g']
+port = int(opts['-p'])
+
+_setup_logging(opts)
+
+if sys.argv[1] == 'recv':
     opts.setdefault('-s',BUFSZ)
-    _setup_logging(opts)
-
-    group = opts['-g']
-    port = int(opts['-p'])
     host = args[0]
+    dst = opts['-o']
 
-    imr = (socket.inet_pton(socket.AF_INET, opts['-g']) +
+    imr = (socket.inet_pton(socket.AF_INET, group) +
            socket.inet_pton(socket.AF_INET, opts['-i']) +
            socket.inet_pton(socket.AF_INET, host))
 
@@ -64,12 +129,12 @@ if sys.argv[1] == 'recv':
     s.setsockopt(socket.SOL_IP, socket.IP_ADD_SOURCE_MEMBERSHIP, imr)
     s.bind((group,port))
 
-    if not os.path.exists(opts['-o']):
-        os.mkfifo(opts['-o'])
+    if not os.path.exists(dst):
+        os.mkfifo(dst)
 
     bufsz = int(opts['-s'])
-    with open(opts['-o'],"w+") as fd:
-        logging.debug("entropy SSM receiver v%s starting..." % VERSION)
+    with open(dst,"w+") as fd:
+        logging.info("entropy SSM receiver v%s starting..." % VERSION)
         while True:
             try:
                 msg = json.loads(s.recv(bufsz))
@@ -86,35 +151,24 @@ if sys.argv[1] == 'recv':
                 time.sleep(z)
 
 elif sys.argv[1] == 'send' or sys.argv[1] == 'rawsend':
-    opts, args = getopt.getopt(sys.argv[2:], 't:s:g:p:r:L:s:')
-    opts = dict(opts)
-    opts.setdefault('-p',SSM_PORT)
-    opts.setdefault('-g',SSM_GROUP)
-    opts.setdefault('-r',ENTROPY_DEVICE)
-    opts.setdefault('-L',LOGLEVEL)
-    opts.setdefault('-t',MCTTL)
     opts.setdefault('-s',MSGSZ)
-
-    _setup_logging(opts)
-
-    group = opts['-g']
-    port = int(opts['-p'])
+    src = opts['-r']
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     if '-t' in opts:
         s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, chr(int(opts['-t'])))
-    if '-s' in opts:
-        s.bind((opts['-s'], 0))
+    if '-i' in opts:
+        s.bind((opts['-i'], 0))
     s.connect(group,port)
     bufsz = int(opts['-s'])
-    with open(opts['-r']) as fd:
+    with open(src) as fd:
         logging.info("entropy SSM transmitter v%s starting..." % VERSION)
         while True:
             try:
                 d = fd.read(bufsz)
                 if sys.argv[1] == 'send':
                    e = base64.b64encode(d)
-                   msg = {'v': PROTOCOL_VERSION,'s':opts['-r'],'d': e}
+                   msg = {'v': PROTOCOL_VERSION,'s':src,'d': e}
                    s.send(json.dumps(msg))
                 else: # rawsend
                    s.send(d)
@@ -122,5 +176,4 @@ elif sys.argv[1] == 'send' or sys.argv[1] == 'rawsend':
             except Exception,ex:
                 logging.warning(ex)
                 pass
-else:
-    raise ValueError("send, rawsend or recv...")
+
